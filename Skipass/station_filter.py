2f5babb0e5732d7feaa6f2eaaf68c6e69cd6ca1d
Jeremie Lecuyer <jeremie_lecuyer@hotmail.fr>
2021-08-30 16:53:07 +0200

    function to filter Alps station from the txt list of all meteo stations

---
+++ Skipass/station_filter.py
@@ -0,0 +1,17 @@
+import folium
+import pandas as pd
+
+def station_filter():
+    df = pd.read_csv('../../documentation/liste_stations_rawdata.txt', delimiter = ",")
+    alps = df.loc[(df['Longitude'] >= 6) & (df['Longitude'] < 7.84)
+                  &
+                  (df['Latitude'] >= 44.48) & (df['Latitude'] <= 46.37)]
+
+    m = folium.Map(location=[45, 6], zoom_start=7)
+    for i in range(0,len(alps)):
+        marker = folium.Marker(
+            location=[alps.iloc[i]['Latitude'], alps.iloc[i]['Longitude']],
+            popup=alps.iloc[i]['Nom'].capitalize(),)
+        marker.add_to(m)
+
+    return m
