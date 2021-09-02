9e443f90c6ca34258825c523cdb954f09f6dccbc
Deva Sou <soudeva974@gmail.com>
2021-09-02 12:42:16 +0200

    setup data, utils and params.py

--- Skipass/data.py
+++ Skipass/data.py
@@ -13,6 +13,8 @@ IMPORTS FROM SKIPASS PACKAGE
 from Skipass.utils.DataCleaner import replace_values,delete_bad_measures,select_stations
 from Skipass.utils.df_typing import mf_date_conv_filtered, mf_date_totime
 from Skipass.station_filter.station_filter import station_filter_nivo,station_filter_synop, station_mapping
+from Skipass.utils.utils import sequence, splitdata
+import Skipass.params as params
 
 """
 PATHS
@@ -24,6 +26,10 @@ class DataSkipass:
 
     def __init__(self):
         self.df = self.create_df()
+        
+    """
+    DATA CREATION
+    """
 
     def import_data(self):
         """
@@ -53,3 +59,32 @@ class DataSkipass:
         df = df_stations.merge(df_data, on='numer_sta')
 
         return df
+    
+    """
+    DATA TRANSFORMATIONS
+    """
+
+    def filter_data(self):
+        df = self.df[self.df.numer_sta.isin(params.Stations)][params.Col_select]
+        df = df.replace("mq",value=np.nan)
+        df = df.replace("/",value=np.nan)
+        df['date'] = pd.to_datetime(df['date'],format='%Y%m%d%H%M%S',errors='coerce')
+     
+        for i in params.col_synop_float:
+            df[i] = df[i].astype(float,errors='ignore')
+
+        return df
+    
+    def split_set(self):
+        return splitdata(self.filter_data())
+    
+    def split_X_y(self):
+        
+        df_train, df_valid, df_test = self.split_set()
+        
+        X_train, y_train = sequence(df_train,params.obs_per_seq,params.target,params.sequence_train)
+        X_valid, y_valid = sequence(df_valid,params.obs_per_seq,params.target,params.sequence_valid)
+        X_test, y_test = sequence(df_test,params.obs_per_seq,params.target,params.sequence_test)
+        
+        return X_train, y_train, X_valid, y_valid, X_test, y_test
+    
