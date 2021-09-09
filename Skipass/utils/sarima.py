1d020390ab33b701763c0a73ae90394d351a3031
Jeremie Lecuyer <jeremie_lecuyer@hotmail.fr>
2021-09-09 09:54:25 +0200

    sarima test

---
+++ Skipass/utils/sarima.py
@@ -0,0 +1,46 @@
+from pmdarima import auto_arima
+from Skipass.data import DataSkipass
+import Skipass.utils
+import numpy as np
+import pickle
+
+
+def sarimax_fourier(df):
+    df = df.replace_nan()
+
+    '''
+    Take df, put date in index then drop
+    y_train = all temp data - last year
+    y_test = last year
+    8 obs/day * 365 = 2920
+    to take into account bisextile years : 8 * 365,25 = 2922
+    '''
+
+    df10 = df.loc[df['numer_sta'] == 7577]
+    df10 = df10[['date', 't']]
+    df10 = df10.set_index(df10['date'])
+    y = df10[['t']]
+    y_to_train = y.iloc[:(len(y) - 2920)]
+    y_to_test = y.iloc[(len(y) - 2920):]  # last year for testing
+
+    '''prepare Fourier terms'''
+    exog = df10
+    exog['sin365'] = np.sin(2 * np.pi * exog.index.dayofyear / 2922)
+    exog['cos365'] = np.cos(2 * np.pi * exog.index.dayofyear / 2922)
+    exog['sin365_2'] = np.sin(4 * np.pi * exog.index.dayofyear / 2922)
+    exog['cos365_2'] = np.cos(4 * np.pi * exog.index.dayofyear / 2922)
+    exog = exog.drop(columns=['date'])
+    exog_to_train = exog.iloc[:(len(y) - 2920)]
+    exog_to_test = exog.iloc[(len(y) - 2920):]
+    # Fit model
+    arima_exog_model = auto_arima(y=y_to_train,
+                                  exogenous=exog_to_train,
+                                  seasonal=True,
+                                  m=2922)
+    # Forecast
+    y_arima_exog_forecast = arima_exog_model.predict(n_periods=2918,
+                                                     exogenous=exog_to_test)
+
+
+    pickle.dump(arima_exog_model, open("sarima_model.p", "wb"))
+    pickle.dump(y_arima_exog_forecast, open("sarima_forecast.p", "wb"))
